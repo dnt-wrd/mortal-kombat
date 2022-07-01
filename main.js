5caec1a3afa76a8574fdb4ee2a7c313d06bd4d6a
dnt-wrd <vladkrivenko1995@yandex.ru>
2022-07-01 14:21:26 +0300

    task2
    2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $arena  = document.querySelector('.arenas');
-
+const $randomButton = document.querySelector('.button');
 
 function Hero(playerNumber, name, hp, img, weapon) {
     this.playerNumber = playerNumber;
@@ -22,8 +22,8 @@ const player1 = new Hero(
 
 const player2 = new Hero(
     2,
-    'SaSUKE',
-    100,
+    'Sakura',
+    2,
     'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     ['SURIKEN', 'kek']
 );
@@ -38,6 +38,8 @@ function createElement(tag, className) {
     return $tag;
 };
 
+
+
 function createPlayer(hero) {
     console.log(hero.hp);
     const $player = createElement('div', 'player'+hero.playerNumber);
@@ -47,8 +49,8 @@ function createPlayer(hero) {
     const $name = createElement('div', 'name');
     const $img = createElement('img');
 
-    $life.innerText = hero.hp;
-    $life.style.width = '100%';
+    //$life.innerText = hero.hp;
+    $life.style.width = hero.hp + '%';
 
     $name.innerText = hero.name;
 
@@ -64,6 +66,82 @@ function createPlayer(hero) {
 
     return $player;
 };
+
+function randomKick(hp) {
+    return Math.ceil(Math.random() * hp);
+}
+
+function changeHp(player) {
+    const $playerLife = document.querySelector('.player'+player.playerNumber + ' .life');
+
+    const hpKick = randomKick(20);
+    console.log(`${hpKick}------- минус`)
+    if (player.hp-hpKick > 0) {
+        player.hp -= hpKick;
+    } else {
+        player.hp = 0;
+    }
+    console.log(player.hp)
+    $playerLife.style.width = player.hp + '%';
+    
+
+    if (player.hp <= 0) {
+        console.log(player.name + ' fffffffffffffff')
+        $arena.appendChild(playerWin(player.playerNumber))
+    }
+}
+
+
+function changeHp(player) {
+    const $playerLife = document.querySelector('.player'+player.playerNumber + ' .life');
+
+    const hpKick = randomKick(20);
+    console.log(`${hpKick}------- минус`)
+    if (player.hp-hpKick > 0) {
+        player.hp -= hpKick;
+    } else {
+        player.hp = 0;
+    }
+    console.log(player.hp)
+    $playerLife.style.width = player.hp + '%';
+    
+
+    if (player.hp <= 0) {
+        $arena.appendChild(playerWin(player.playerNumber))
+    }
+}
+
+// функция из урока
+// function playerLose(name) {
+//     const $loseTitle = createElement('div', 'loseTitle');
+//     $loseTitle.innerText = name + ' lose';
+//     return $loseTitle;
+// }
+
+function playerWin(plNum) {
+    console.log(player1.name + " " + player2.name)
+    let name = '';
+    if (plNum === 1) {
+      name = player2.name;  
+    } else {
+      name = player1.name;
+    }
+    const $loseTitle = createElement('div', 'loseTitle');
+    $loseTitle.innerText = name + ' win';
+
+    return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function(){
+    changeHp(player1);
+    changeHp(player2);
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        console.log(player1.hp + ' ' + player2.hp)
+        $randomButton.disabled = true;
+        console.log('kek')
+    }
+})
 
 $arena.appendChild(createPlayer(player1));
 $arena.appendChild(createPlayer(player2));
