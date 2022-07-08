fbcce704e806dab60c2a841504396593ee27a25f
dnt-wrd <vladkrivenko1995@yandex.ru>
2022-07-09 00:01:28 +0300

    fic less4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const $arena  = document.querySelector('.arenas');
-
+const $randomButton = document.querySelector('.button');
 
 function Hero(playerNumber, name, hp, img, weapon) {
     this.playerNumber = playerNumber;
@@ -15,7 +15,7 @@ function Hero(playerNumber, name, hp, img, weapon) {
 const player1 = new Hero(
     1,
     'Naruto',
-    100,
+    10,
     'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     ['lol', 'kek']
 );
@@ -23,7 +23,7 @@ const player1 = new Hero(
 const player2 = new Hero(
     2,
     'SaSUKE',
-    100,
+    1,
     'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     ['SURIKEN', 'kek']
 );
@@ -38,6 +38,10 @@ function createElement(tag, className) {
     return $tag;
 };
 
+function getRandom(maxDamage) {
+    return Math.ceil(Math.random() * maxDamage);
+}
+
 function createPlayer(hero) {
     console.log(hero.hp);
     const $player = createElement('div', 'player'+hero.playerNumber);
@@ -47,8 +51,8 @@ function createPlayer(hero) {
     const $name = createElement('div', 'name');
     const $img = createElement('img');
 
-    $life.innerText = hero.hp;
-    $life.style.width = '100%';
+    //$life.innerText = hero.hp;
+    $life.style.width = hero.hp + '%';
 
     $name.innerText = hero.name;
 
@@ -64,6 +68,52 @@ function createPlayer(hero) {
 
     return $player;
 };
+
+
+
+
+function playerWins(name) {
+    const $loseTitle = createElement('div', 'winTitle');
+
+    if (name) {
+        $loseTitle.innerText = name + ' wins';
+    } else {
+        $loseTitle.innerText = 'draw';
+    }
+    
+
+    return $loseTitle;
+}
+
+function changeHP(player){
+    const $playerLife = document.querySelector('.player' + player.playerNumber + ' .life');
+    player.hp -= getRandom(20);
+
+    $playerLife.style.width = player.hp + '%';
+
+    if (player.hp <= 0) {
+        player.hp = 0;
+    }
+
+    $playerLife.style.width = player.hp + '%';
+}
+
+$randomButton.addEventListener('click', function(){
+    changeHP(player1);
+    changeHP(player2);
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true;
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arena.appendChild(playerWins(player2.name));
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arena.appendChild(playerWins(player1.name));
+    } else if (player1.hp === 0 && player2.hp === 0) {
+        $arena.appendChild(playerWins());
+    }
+})
 
 $arena.appendChild(createPlayer(player1));
 $arena.appendChild(createPlayer(player2));
